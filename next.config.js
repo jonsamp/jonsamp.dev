d3a8e47cdc7a375d3e078254432f7f6b69e64f7c
Jon Samp <sampjon@gmail.com>
2025-02-16 12:28:39 -0600

    save

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/.well-known/appspecific/com.tesla.3p.public-key.pem',
-        destination: '/api/.well-known/appspecific/com.tesla.3p.public-key.pem'
-      }
-    ];
-  }
+  output: 'export',
+  // Ignore API routes in static export
+  distDir: '.next',
+  images: {
+    unoptimized: true,
+  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/.well-known/appspecific/com.tesla.3p.public-key.pem',
+  //       destination: '/api/.well-known/appspecific/com.tesla.3p.public-key.pem'
+  //     }
+  //   ];
+  // }
 };
 
 module.exports = nextConfig; 
